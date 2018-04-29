511d10404a9e25194e742bb9c5a30029941efaf4
Austin Brady <drone31234@gmail.com>
2018-04-29 11:01:59 -0400

    name change

--- umbc-app/src/app/hero-detail/hero-detail.component.ts
+++ umbc-app/src/app/hero-detail/hero-detail.component.ts
@@ -35,67 +35,6 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
-  wilksCalc() {
-    var sex = prompt("Are you a male or a female?");
-    if (sex != null) {
-      while ((sex != ('Male').toString()) && (sex != ('male').toString()) &&
-      (sex != ('Female').toString()) && (sex != ('female').toString())) {
-        if (sex == null) {return;}
-        else {sex = prompt("Please enter a valid sex");}
-      }
-      var weightString = prompt("Please enter your weight (in lbs)");
-      if (weightString != null) {
-        while (parseFloat(weightString) < 0 || weightString == '') {
-          weightString = prompt("Please enter a valid weight");
-        } if (weightString == null) {return;}
-        var weight = parseFloat(weightString)/2.2;
-        var squatString = prompt("Please enter your 1rm squat (in lbs)");
-        if (squatString != null) {
-          while (parseFloat(squatString) < 0 || squatString == '') {
-            squatString = prompt("Please enter a valid 1rm squat");
-          } if (squatString == null) {return;}
-          var squat = parseFloat(squatString)/2.2;
-          var benchString = prompt("Please enter your 1rm bench (in lbs)");
-          if (benchString != null) {
-            while (parseFloat(benchString) < 0 || benchString == '') {
-              benchString = prompt("Please enter a valid 1rm bench");
-            } if (benchString == null) {return;}
-            var bench = parseFloat(benchString)/2.2;
-            var deadliftString = prompt("Please enter your 1rm deadlift (in lbs)");
-            if (deadliftString != null) {
-              while (parseFloat(deadliftString) < 0 || deadliftString == '') {
-                deadliftString = prompt("Please enter a valid 1rm deadlift");
-              } if (deadliftString == null) {return;}
-              var deadlift = parseFloat(deadliftString)/2.2;
-              var total = squat+bench+deadlift;
-              var wilksCoefficient, a, b, c, d, e, f;
-              if ((sex == 'Female') || (sex == 'female')) {
-                a=594.31747775582;
-                b=-27.23842536447;
-                c=0.82112226871;
-                d=-0.00930733913;
-                e=0.00004731582;
-                f=-0.000000009054;
-                wilksCoefficient = 500/(a+(b*weight)+(c*(weight^2))+
-                  (d*(weight^3))+(e*(weight^4))+(f*(weight^5)));
-              }
-              else {
-                a=-216.0475144;
-                b=16.2606339;
-                c=-0.002388645;
-                d=-0.00113732;
-                e=0.00000701863;
-                f=-0.00000001291;
-                wilksCoefficient = 500/(a+(b*weight)+(c*(Math.pow(weight,2)))+
-                  (d*(Math.pow(weight,3)))+(e*(Math.pow(weight,4)))+(f*(Math.pow(weight,5))));
-              }
-              alert("Your wilks is " + (total*wilksCoefficient));
-            } else {return;}
-          } else {return;}
-        }else {return;}
-      } else {return;}
-    } else {return;}
-  }
   chart() {
     var chart;
     const id = +this.route.snapshot.paramMap.get('id');
