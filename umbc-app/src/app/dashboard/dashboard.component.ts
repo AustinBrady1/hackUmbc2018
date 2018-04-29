f826e51ff2512f227edd2cebc975b3ecdcde232d
Austin Brady <drone31234@gmail.com>
2018-04-29 00:02:29 -0400

    THE FUCKING GRAPH

--- umbc-app/src/app/dashboard/dashboard.component.ts
+++ umbc-app/src/app/dashboard/dashboard.component.ts
@@ -20,4 +20,42 @@ export class DashboardComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 4));
   }
+  // wilksCalc() {
+  //   alert("hello");
+  //   var sex = prompt("Are you a male or a female?");
+  //   if (sex != null) {
+  //     while (sex != "Male" || sex != "male" || sex != "Female" || sex != "female") {
+  //       sex = prompt("Please enter a valid sex");
+  //     }
+  //     var weight = prompt("Please enter your weight (in lbs)");
+  //     if (weight != null) {
+  //       while (weight < 0) {
+  //         weight = prompt("Please enter a valid weight");
+  //       }
+  //       weight = parseFloat(weight)/2.2;
+  //       var wilks, a, b, c, d, e, f;
+  //       if (sex === "Female" || sex === "female") {
+  //         a=594.31747775582;
+  //         b=-27.23842536447;
+  //         c=0.82112226871;
+  //         d=-0.00930733913;
+  //         e=4.731582*(10^-5);
+  //         f=-9.054*(10^-8);
+  //         wilks = 500/(a+(b*weight)+(c*(weight^2))+
+  //           (d*(weight^3))+(e*(weight^4))+(f*(weight^5)));
+  //       }
+  //       else {
+  //         a=-216.0475144;
+  //         b=16.2606339;
+  //         c=-0.002388645;
+  //         d=-0.00113732;
+  //         e=7.01863*(10^-6);
+  //         f=-1.291*(10^-8);
+  //         wilks = 500/(a+(b*weight)+(c*(weight^2))+
+  //           (d*(weight^3))+(e*(weight^4))+(f*(weight^5)));
+  //       }
+  //       alert("Your wilks is " + wilks);
+  //     }
+  //   }
+  // }
 }
--- umbc-app/src/app/heroes/heroes.component.ts
+++ umbc-app/src/app/heroes/heroes.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// import CanvasJS from 'canvasjs';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+declare var CanvasJS: any;
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
+  template: '<div id="chartContainer" style="height: 370px; width: 100%;"></div>',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
@@ -15,10 +18,48 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
     this.getHeroes();
+    this.chart();
   }
-
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
+  chart() {
+      var chart = new CanvasJS.Chart("chartContainer", {
+      	animationEnabled: true
+        ,
+
+      	title:{
+      		text:"Squat"
+      	},
+      	axisX:{
+      		interval: 1
+      	},
+      	axisY2:{
+      		interlacedColor: "rgba(1,77,101,.2)",
+      		gridColor: "rgba(1,77,101,.1)",
+      		title: "Weight in lbs"
+      	},
+      	data: [{
+      		type: "bar",
+      		name: "lifters",
+      		axisYType: "secondary",
+      		color: "#014D65",
+      		dataPoints: [
+      			{ y: 639, label: "Andrzej Stanaszek" },
+      			{ y: 551, label: "Mike Booker" },
+      			{ y: 556, label: "Mike Kuhns" },
+      			{ y: 622, label: "Justin Caputo" },
+      			{ y: 749, label: "Aleksey Nikulin" },
+      			{ y: 804, label: "Amit Sapir" },
+      			{ y: 821, label: "Amit Sapir" },
+            { y: 832, label: "Kevin Oak" },
+            { y: 865, label: "Dennis Cornelius" },
+            { y: 920, label: "Eric Lilliebridge "},
+            { y: 1052, label: "Ray Williams"}
+      		]
+      	}]
+      });
+      chart.render();
+    }
 }
